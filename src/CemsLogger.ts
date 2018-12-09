22ff9fbf6b49523f4ea0ca72c90ad858b1d2c0b3
Branislav <branislav.bellovic@sde.cz>
2018-12-09 21:36:57 +0100

    Minor changes in data strucutre

--- src/CemsLogger.ts
+++ src/CemsLogger.ts
@@ -3,17 +3,11 @@ import { InitializationError } from "./InitializationError";
 
 let instance: CemsLogger;
 
-let ip = "";
-fetch("https://api.ipify.org/?format=json")
-  .then(res => res.json)
-  .then((res: any) => (ip = res.ip));
-
 const defaultOptions = {
   apiKey: "",
   appName: "unknown",
   email: "",
-  endPointUrl: "http://localhost:5000/",
-  ip: "unknown"
+  endPointUrl: "http://localhost:5000/"
 };
 
 export class CemsLogger {
@@ -29,6 +23,7 @@ export class CemsLogger {
   private readonly apiKey: string;
   private readonly appName: string;
   private readonly email: string;
+  private ip!: string;
   private session: CemsSession;
 
   private constructor(options: any) {
@@ -39,8 +34,9 @@ export class CemsLogger {
     }
     this.apiKey = options.apiKey;
     this.appName = options.appName || defaultOptions.appName;
-    this.email = options.appName || undefined;
+    this.email = options.email || undefined;
     this.session = new CemsSession();
+    this.ip = this.getIp();
     this.healthCheck();
   }
 
@@ -79,11 +75,21 @@ export class CemsLogger {
   public errorLogFromError(error: Error) {
     return {
       name: error.name,
-      source: name,
+      source: this.appName,
+      email: this.email,
+      ip: this.ip,
       message: error.message,
       stacktrace: error.stack,
       timestamp: new Date().toLocaleString(),
-      sessionData: this.session.getClientInfo()
+      sessionInfo: this.session.getClientInfo()
     };
+  }
+
+  private getIp(): string {
+    const request = new XMLHttpRequest();
+    request.open("GET", "https://api.ipify.org/?format=json", false); // `false` makes the request synchronous
+    request.send(null);
+    const json = JSON.parse(request.responseText);
+    return json.ip;
   }
 }
